1d15d27c0e9d37bd33b4c52ca02a993ba1e2e00c
TheJLifeX <44620382+TheJLifeX@users.noreply.github.com>
2021-12-19 22:12:52 +0100

    Try to handle right-click on back- and forward button.

--- projects/ngx-navigation-trigger/src/lib/navigation-trigger.service.ts
+++ projects/ngx-navigation-trigger/src/lib/navigation-trigger.service.ts
@@ -4,6 +4,12 @@ import { Observable, Subject } from 'rxjs';
 import { NavigationTrigger } from './navigation-trigger';
 
 /**
+ * To goal here (in this branch) is to be able to handle right-click on back and forward button and then click on any previous navigation entity.
+ * But this does not work currently.
+ * Reasons:
+ * - Browser creates navigation item with new id on popstate.
+ */
+/**
  * The `NavigationTriggerService` allows you to know if the current page was trigged by either an `'imperative'`, `'backward'` or `'forward'` navigation.
  */
 @Injectable()
@@ -15,6 +21,7 @@ export class NavigationTriggerService {
   eventUrlChanged: Observable<NavigationTrigger>;
 
   private forwardNavigationHistory: NavigationItem[] = [];
+  private backwardNavigationHistory: NavigationItem[] = [];
   private navigationTriggerSubject: Subject<NavigationTrigger> = new Subject();
   private currentHighestNavigationId: number = 0;
   private previousNavigationItem!: NavigationItem;
@@ -34,43 +41,47 @@ export class NavigationTriggerService {
 
   private onEventPopState(url: string, state: { navigationId: number }) {
     // Back or forward navigation
-    const { navigationId } = state;
+    const { navigationId } = state as { navigationId: number };
     let backward: boolean;
-    const mostRecentForwardNavigationItem = this.forwardNavigationHistory[0];
     const forwardOrBackwardItem: NavigationItem = {
       url: url!,
       navigationId
     };
-    if (!mostRecentForwardNavigationItem || !this.compareNavigationItem(mostRecentForwardNavigationItem, forwardOrBackwardItem)) {
+    const index = this.forwardNavigationHistory.findIndex((item) => this.compareNavigationItem(item, forwardOrBackwardItem));
+    if (index < 0) {
       backward = true;
-      const currentNavigationItemBeforeBackorForwardNavigation = this.previousNavigationItem;
-      this.forwardNavigationHistory.unshift(currentNavigationItemBeforeBackorForwardNavigation);
+      console.log('backwardNavigationHistory', JSON.parse(JSON.stringify(this.backwardNavigationHistory)));
+      const indexx = this.backwardNavigationHistory.findIndex((item) => this.compareNavigationItem(item, forwardOrBackwardItem));
+      const rest = this.backwardNavigationHistory.splice(indexx + 1);
+      console.log('backwardNavigationHistory', JSON.parse(JSON.stringify(this.backwardNavigationHistory)));
+      this.forwardNavigationHistory.unshift(...rest);
+      console.log('forwardNavigationHistory', JSON.parse(JSON.stringify(this.forwardNavigationHistory)));
     }
     else {
       backward = false;
-      this.forwardNavigationHistory.shift();
+      const rest = this.forwardNavigationHistory.splice(0, index + 1);
+      this.backwardNavigationHistory.push(...rest);
     }
     this.navigationTriggerSubject.next(backward ? NavigationTrigger.BACKWARD : NavigationTrigger.FORWARD);
   }
 
   private onEventUrlChanged(url: string, state: { navigationId: number }): void {
-    const { navigationId } = state;
-
-    const isImperativeNavigation =
-      typeof this.currentHighestNavigationId === 'undefined'
-      || (this.currentHighestNavigationId < navigationId && url !== this.previousNavigationItem?.url);
-
-    if (isImperativeNavigation) {
+    const { navigationId } = state as { navigationId: number };
+    const currentNavigationItem: NavigationItem = {
+      url,
+      navigationId
+    };
+    // Imperative navigation
+    if (typeof this.currentHighestNavigationId === 'undefined'
+      || (this.currentHighestNavigationId < navigationId && url !== this.previousNavigationItem?.url)) {
       this.forwardNavigationHistory = [];
+      this.backwardNavigationHistory.push(currentNavigationItem);
       this.navigationTriggerSubject.next(NavigationTrigger.IMPERATIVE);
     }
 
     this.currentHighestNavigationId = typeof this.currentHighestNavigationId === 'undefined' ? navigationId : Math.max(this.currentHighestNavigationId, navigationId);
 
-    this.previousNavigationItem = {
-      url,
-      navigationId
-    };
+    this.previousNavigationItem = currentNavigationItem;
   }
 
   /**
